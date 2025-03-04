b6b7aace5aa5e80b70ead33d6d0d53c3b2562a58
WendelWeb <159454536+WendelWeb@users.noreply.github.com>
2025-03-04 06:34:13 -0800

    edit routes

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,4 +1,4 @@
-import IndexAdmin from "@/admin/Index"; // ou "@/admin/index" selon le fichier réel
+import IndexAdmin from "../admin/Index"; // ou "@/admin/index" selon le fichier réel
 import AdminPage from "@/admin/AdminPage";
 import AddShipment from "@/admin/add-shipment/AddShipment";
 import AboutPage from "@/pages/About";
