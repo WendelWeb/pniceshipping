a6ee4ca2bfc8fd5e5bed64919bf4d64a10b971b0
WendelWeb <159454536+WendelWeb@users.noreply.github.com>
2025-05-21 15:24:16 -0700

    correct hadle input

--- src/admin/add-shipment/InputField.tsx
+++ src/admin/add-shipment/InputField.tsx
@@ -1,52 +1,28 @@
-// import { Input } from "../../components/ui/input";
-// interface InputFieldProps {
-//   handleIputChange: (name: string, value: string | number) => void;
-//   item: {
-//     name: string;
-//     fieldType: string;
-//     placeholder?: string;
-//     required?: boolean;
-//     label?: string;
-//     min?: number;
-//     max?: number;
-//   };
-//   defaultValue?: string;
-// }
-// const InputField = ({ handleIputChange, item,defaultValue }: InputFieldProps) => {
-//   return (
-//     <div>
-//       <Input
-//       defaultValue={defaultValue? defaultValue : ''}
-//         type={item?.fieldType}
-//         name={item?.name}
-//         required={item?.required}
-//         onChange={(e) => handleIputChange(e.target.name, e.target.value)}
-//       />
-//     </div>
-//   );
-// };
-
-// export default InputField;
-// InputField.tsx
-import { ShipmentFormItem } from "@/types/shipment.ts";
-import { FC } from "react";
-
+import { Input } from "../../components/ui/input";
 interface InputFieldProps {
-  item: ShipmentFormItem;
-  defaultValue: string;
-  handleInputChange: (name: string, value: any) => void;
+  handleInputChange: (name: string, value: string | number) => void;
+  item: {
+    name: string;
+    fieldType: string;
+    placeholder?: string;
+    required?: boolean;
+    label?: string;
+    min?: number;
+    max?: number;
+  };
+  defaultValue?: string;
 }
-
-const InputField: FC<InputFieldProps> = ({ item, defaultValue, handleInputChange }) => {
+const InputField = ({ handleInputChange, item,defaultValue }: InputFieldProps) => {
   return (
-    <input
-      type={item.fieldType}
-      name={item.name}
-      value={defaultValue}
-      onChange={(e) => handleInputChange(item.name, e.target.value)}
-      required={item.required}
-      className="mt-1 p-2 w-full border rounded"
-    />
+    <div>
+      <Input
+      defaultValue={defaultValue? defaultValue : ''}
+        type={item?.fieldType}
+        name={item?.name}
+        required={item?.required}
+        onChange={(e) => handleInputChange(e.target.name, e.target.value)}
+      />
+    </div>
   );
 };
 
